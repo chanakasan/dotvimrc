33351169cd734e35b2dba73a69b6e37e327dd012
Chanaka Abeysinghe <chanakasan@gmail.com>
2023-05-23 12:53:09 +0530

    Update install-win.sh

--- install-win.sh
+++ install-win.sh
@@ -7,3 +7,8 @@ powershell.exe "cmd.exe /c mklink %HOME%\.gvimrc %HOME%\chk-vimrc\gvimrc.vim"
 powershell.exe "cmd.exe /c mklink /d %HOME%\vimfiles %HOME%\.vim"
 powershell.exe "cmd.exe /c mklink /d %HOME%\.vim\colors %HOME%\chk-vimrc\colors"
 powershell.exe "cmd.exe /c mklink /d %HOME%\.vim\plugin %HOME%\chk-vimrc\plugin"
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo "Done"
+echo ""
