488b9fc58080b5ed5bdfc6e648f06de906bdc05a
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-01-06 21:14:49 +0530

    rev

--- setup/install-win.sh
+++ setup/install-win.sh
@@ -1,14 +1,16 @@
 # Run from Git Bash
 # sh install-win.sh
 
-powershell.exe "mkdir -p \$env:HOME\.vim"
-powershell.exe "cmd.exe /c mklink %HOME%\.vimrc %HOME%\chk-vimrc\vimrc.vim"
-powershell.exe "cmd.exe /c mklink %HOME%\.gvimrc %HOME%\chk-vimrc\gvimrc.vim"
-powershell.exe "cmd.exe /c mklink /d %HOME%\vimfiles %HOME%\.vim"
-powershell.exe "cmd.exe /c mklink /d %HOME%\.vim\colors %HOME%\chk-vimrc\colors"
-powershell.exe "cmd.exe /c mklink /d %HOME%\.vim\plugin %HOME%\chk-vimrc\plugin"
+main() {
+  powershell.exe "mkdir -p \$env:HOME\.vim"
+  powershell.exe "cmd.exe /c mklink %HOME%\.vimrc %HOME%\nex-dotvim\src\vimrc.vim"
+  powershell.exe "cmd.exe /c mklink %HOME%\.gvimrc %HOME%\nex-dotvim\src\gvimrc.vim"
+  powershell.exe "cmd.exe /c mklink /d %HOME%\vimfiles %HOME%\.vim"
+  powershell.exe "cmd.exe /c mklink /d %HOME%\.vim\colors %HOME%\nex-dotvim\src\colors"
+  powershell.exe "cmd.exe /c mklink /d %HOME%\.vim\plugin %HOME%\nex-dotvim\src\plugin"
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-echo "Done"
-echo ""
+  echo "Done"
+  echo ""
+}
