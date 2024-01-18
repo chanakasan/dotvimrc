c4df5965a2cf319a9b6eb88568f360f65e5dd912
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-01-18 09:54:01 +0530

    rev

--- setup/install-nix.sh
+++ setup/install-nix.sh
@@ -1,13 +1,19 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 main() {
-  echo " Installing - Nex Vim"
+  local title="Nex Vim"
+  local dir_name="nex-vim"
+  local start_text='__nex_vim_start'
+  local end_text='__nex_vim_end'
+  local bashrc="$HOME/.bashrc"
+  local nex_vim_path=$(get_root_path)/$dir_name
+  echo " Installing - $title"
   
-  ln -nfs $HOME/nex-vim/src/vimrc.vim $HOME/.vimrc 
-  ln -nfs $HOME/nex-vim/src/gvimrc.vim $HOME/.gvimrc 
+  ln -nfs $nex_vim_path/src/vimrc.vim $HOME/.vimrc 
+  ln -nfs $nex_vim_path/src/gvimrc.vim $HOME/.gvimrc 
   mkdir -p $HOME/.vim
-  ln -nfs $HOME/nex-vim/src/colors $HOME/.vim/colors 
-  ln -nfs $HOME/nex-vim/src/plugin $HOME/.vim/plugin 
+  ln -nfs $nex_vim_path/src/colors $HOME/.vim/colors 
+  ln -nfs $nex_vim_path/src/plugin $HOME/.vim/plugin 
 
   install_vim_plug
 
